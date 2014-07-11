941241ce3cbde5766e538b74d0fd14b570f6f09b
iavian <iavian@iavians-MacBook-Pro.local>
2014-07-11 07:02:41 -0400

    CreateChallenge

---
+++ imagematch/SolveChallengeViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface SolveChallengeViewController : UIViewController
+
+@end
---
+++ imagematch/SolveChallengeViewController.m
@@ -0,0 +1,26 @@
+#import "SolveChallengeViewController.h"
+
+@interface SolveChallengeViewController ()
+
+@end
+
+@implementation SolveChallengeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
